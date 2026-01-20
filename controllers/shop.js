7b42cdc4e1a7f842698d515531c530f7f862d18e
Rikuto <mikadorikuto@gmail.com>
2026-01-20 22:35:31 +0900

    update the code

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,12 +10,16 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
+// Get a single product by ID and render detail page
 exports.getProduct = (req, res, next) => {
     const prodId = req.params.productId;
     Product.findById(prodId, (product) => {
-        console.log(product);
+        res.render('shop/product-detail', {
+            product: product,
+            pageTitle: product.title,
+            path: '/products'
+        })
     });
-    res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {

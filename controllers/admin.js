1ee6f26ea05c6f8d8c4610b9da288dbb6ef8d65a
Rikuto <mikadorikuto@gmail.com>
2026-02-06 22:57:40 +0900

    update the code

--- controllers/admin.js
+++ controllers/admin.js
@@ -21,10 +21,26 @@ exports.postAddProduct = (req, res, next) => {
 
 // Edit product page handler
 exports.getEditProduct = (req, res, next) => {
-    res.render('admin/edit-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    // Find the product by ID using Array.find() to get the specific product to edit
+    // findById is a static method in Product model that reads all products and filters by ID
+    // Other array selection methods: filter() (returns all matches), findIndex() (returns index), some() (returns boolean)
+    Product.findById(prodId, product => {
+        if (!product) {
+            return res.redirect('/');
+        }
+        res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        // Pass the edit query param to tell the template if we're in edit mode
+        editing: editMode,
+        product: product
     });
+    })
 };
 
 exports.getProducts = (req, res, next) => {
